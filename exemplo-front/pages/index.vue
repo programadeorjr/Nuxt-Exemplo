9cc28080f71b6db34a9fadd6f149e2d7a00fa894
unknown <leny.dinha@hotmail.com>
2022-05-02 16:53:27 -0300

    Adicionando arquivo

---
+++ exemplo-front/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Bem vindo</h1>
+    <ul>
+      <li><a href="lista_pessoa">Elemento1</a></li>
+      <li>Elemento2</li>
+      <li>Elemento3</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+
+</style>
---
+++ exemplo-front/pages/lista_pessoa.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+      <h1>{{titulo}}</h1>
+  <ul>
+      <li :key="item.id" v-for = "item in pessoas">
+          <!-- Saída: Nome: Marcio -> 18 anos -->
+      {{item.nome}} -> {{item.idade}}</li>
+  </ul>
+  <button>Clique aqui</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function (){
+        return{
+            titulo:"Lista de peassoas e idades",
+            pessoas: [
+                {id: 1,nome:"Pedro", idade:18},
+                {id: 1,nome:"Maria", idade:13},
+                {id: 1,nome:"Clara", idade:29},
+                {id: 1,nome:"Ana", idade:17},
+                {id: 1,nome:"Mayara", idade:23},
+                {id: 1,nome:"Laura", idade:15},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
